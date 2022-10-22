6885e1fde117063d19e760d50522424fc17660c9
Daniel Noworyta <daniel.noworyta@gmail.com>
2022-10-22 17:10:13 +0200

    Repair TS errors in main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (!process.env.PORT) {
+    return;
+  }
   await app.listen(process.env.PORT);
   console.log(`My server is running on port ${process.env.PORT}`);
 }
