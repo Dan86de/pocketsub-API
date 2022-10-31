37947c735a4906058134c6037e02c2c7c219c791
Daniel Noworyta <daniel.noworyta@gmail.com>
2022-10-31 12:20:39 +0100

    Add user, subscription and notification models

--- src/config/envValidation.config.ts
+++ src/config/envValidation.config.ts
@@ -3,9 +3,5 @@ import * as Joi from 'joi';
 export const envValidationSchema = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production', 'test'),
   PORT: Joi.number().default(4000),
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().default(5432),
-  DB_NAME: Joi.string().required(),
-  DB_USER: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
+  DATABASE_URL: Joi.string().required(),
 });
