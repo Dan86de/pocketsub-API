1280195ddaaaec7d7b61cff1979eab0bb6f5466c
Daniel Noworyta <daniel.noworyta@gmail.com>
2022-10-31 12:51:37 +0100

    Add prisma service to project

---
+++ prisma/migrations/20221031114722_init/migration.sql
@@ -0,0 +1,58 @@
+-- CreateEnum
+CREATE TYPE "Interval" AS ENUM ('MONTHLY', 'YEARLY');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "full_name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar_url" TEXT NOT NULL DEFAULT 'https://avatar.com',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "tags" TEXT[],
+    "interval" "Interval" NOT NULL,
+    "monthly_price" INTEGER NOT NULL,
+    "yearly_price" INTEGER NOT NULL,
+    "period_start" TIMESTAMP(3) NOT NULL,
+    "period_end" TIMESTAMP(3) NOT NULL,
+    "is_canceled" BOOLEAN DEFAULT false,
+    "canceled_at" TIMESTAMP(3),
+    "active_until" TIMESTAMP(3),
+    "expires_in" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "text" TEXT,
+    "notify_at" TIMESTAMP(3) NOT NULL,
+    "subscription_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if (!process.env.PORT) {
     return;
   }
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(process.env.PORT);
   console.log(`My server is running on port ${process.env.PORT}`);
 }
